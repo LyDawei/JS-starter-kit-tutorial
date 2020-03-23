86ac7af1b18bd2f77894760c8acc7a515309f380
David Ly <david.ly@transcore.com>
2020-03-22 20:19:09 -0500

    setup webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
-var port = 3000;
+const port = 3000;
 
-var app = express();
+const app = express();
 
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, '../src/index.html')) });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
 
 app.listen(port, (err) => {
     if (err) {
